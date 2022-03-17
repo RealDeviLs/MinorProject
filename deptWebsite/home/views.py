4064e4020f1d0ab25ffbf662aebab7e5775ac275
Rahula-12 <66356496+Rahula-12@users.noreply.github.com>
2022-03-17 15:36:49 +0530

    updated home/views.py

--- deptWebsite/home/views.py
+++ deptWebsite/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.sites.shortcuts import get_current_site
 from django.conf import settings
+from academics.models import AcademicProgram
 # Create your views here.
 from whitelabel.models import WhiteLabel
 
@@ -8,4 +9,10 @@ from whitelabel.models import WhiteLabel
 def home_page(request):
     print(get_current_site(request))
     print(settings.SITE_ID)
-    return render(request,template_name="index.html")
+    academic_program =AcademicProgram.on_site.all()[0]
+    peo = academic_program.peo.all()
+    btech=peo[0]
+    mtech=peo[1]
+    phd=peo[2]
+    print(btech.description)
+    return render(request,template_name="index.html",context={'btech':btech,'mtech':mtech,'phd':phd})
