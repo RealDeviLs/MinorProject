d8cc25f7fbaca356ba2aca5578164387d65965d0
sanujsood <67072668+sanujsood@users.noreply.github.com>
2022-05-18 22:46:07 +0530

    contact form

--- deptWebsite/home/views.py
+++ deptWebsite/home/views.py
@@ -28,28 +28,20 @@ def home_page(request):
 
 def contact(request):
     if request.method == "POST":
+        print("hey hey")
         name = request.POST["full-name"]
         email = request.POST["email"]
         phone = request.POST["phone"]
         message = request.POST["message"]
         reciever = []
-        reciever.append("atmproject.sem3@.com")
-        send_mail(
-            f"""
-                New query from  Website \n
-                Name : {name} \n
-                email : {email} \n
-                Phone : {phone} \n
-                message : {message} \n
+        reciever.append("atmproject.sem3@gmail.com")
+        print(reciever)
+        send_mail("New query from  Website ","\n name: "+name+"\n email: "+email+"\n phone: "+phone+"\n message: "+message+ "\n","ra9853044@gmail.com", reciever)
 
-             """,
-            EMAIL_HOST_USER,
-            reciever,
-        )
         messages.success(request, "Your query has been submitted")
         return HttpResponseRedirect(request.path_info)
 
-    return render(request, "base.html")
+    return render(request, "index.html")
 
 
 def show_news(request):
